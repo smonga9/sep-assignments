e05dbd32dba04484294520583842d6bc5f985bad
Stutay Monga <Stutay.Monga9@gmail.com>
2018-11-16 06:37:45 -0500

    Completed Stacks and Queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -1,18 +1,30 @@
 class MyQueue
   attr_accessor :head
   attr_accessor :tail
+  attr_accessor :queue
 
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    first_person = @queue.delete_at(0)
+    return first_person
   end
 
   def empty?
+    if @queue.length === 0
+      return true
+    else
+      return false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -1,5 +1,6 @@
 class MyStack
   attr_accessor :top
+  attr_accessor :stack
 
   def initialize
     @stack = Array.new
@@ -7,11 +8,21 @@ class MyStack
   end
 
   def push(item)
+    self.top = item
+    @stack << item
   end
 
   def pop
+    last_person = @stack.delete_at(-1)
+    self.top = @stack[-1]
+    return last_person
   end
 
   def empty?
+    if @stack.length === 0
+      return true
+    else
+      return false
+    end
   end
 end
