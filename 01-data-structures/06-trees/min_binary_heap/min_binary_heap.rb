7676d2f1c65af09fb8fc8d81c79f3755a87fa48a
Stutay Monga <Stutay.Monga9@gmail.com>
2018-12-26 16:47:06 -0500

    Completed Trees

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -1,20 +1,32 @@
 require_relative 'node'
 
-class BinarySearchTree
+class MinBinaryHeap
+  attr_reader :root
 
   def initialize(root)
     @root = root
   end
 
-  def insert(root, node)
-    if root.rating > node.rating
-      root.left.nil? ? (root.left = node) : insert(root.left, node)
-    else
-      root.right.nil? ? (root.right = node) : insert(root.right, node)
+def insert(root, node)
+
+  if root.rating > node.rating
+    temp = root
+    @root = node
+    node = temp
+    insert(@root, node)
+  else
+    if root.left.nil?
+      root.left = node
+    elsif root.right.nil? && root.left != nil
+      root.right = node
+    elsif root.left != nil && root.right != nil && root.left.left != nil && root.left.right != nil
+      insert(root.right, node)
+    elsif root.left != nil && root.right != nil
+      insert(root.left, node)
     end
   end
+end
 
-  # Recursive Depth First Search
   def find(root, data)
     if root.nil? || data.nil?
       return nil
@@ -38,8 +50,7 @@ class BinarySearchTree
     end
   end
 
-  # Recursive Breadth First Search
-  def printf(children=nil)
+  def printf
     queue = [@root]
     result = []
     while queue.length > 0
