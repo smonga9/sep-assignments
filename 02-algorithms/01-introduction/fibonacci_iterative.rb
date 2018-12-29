dfb816e0f2ec621171dabdb8e2c2eb419f142824
Stutay Monga <Stutay.Monga9@gmail.com>
2018-12-29 16:57:27 -0500

    Updated Intro To Algorithms

--- 02-algorithms/01-introduction/fibonacci_iterative.rb
+++ 02-algorithms/01-introduction/fibonacci_iterative.rb
@@ -1,17 +1,19 @@
 def fib(n)
-  if n == 0
+  if n === 0
     return 0
-  else
-    fib_0 = 0
-    fib_1 = 1
-    (1..(n-1)).each do
-      temp = fib_0
-      fib_0 = fib_1
-      fib_1 = temp + fib_1
-    end
-    return fib_1
   end
+
+  fib_0 = 0
+  fib_1 = 1
+
+  for i in 1..(n-1)
+    temp = fib_0
+    fib_0 = fib_1
+    fib_1 = temp + fib_1
+  end
+  return fib_1
 end
+
 
 
 puts fib(0)
